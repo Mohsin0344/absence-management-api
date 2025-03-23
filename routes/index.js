1a5d6585bb383153107cdab2c7bf1712774178b0
Mohsin Elahi <66850855+Mohsin0344@users.noreply.github.com>
2025-03-23 21:48:47 +0500

    Added route handlers
    Added route handlers for members, absences and unique absence types in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { getMembers } = require("../controllers/members");
+const { getAbsences } = require("../controllers/absence");
+const { getUniqueAbsenceTypes } = require("../controllers/uniqueAbsenceTypes");
 
 const ROUTES = {
-
+    MEMBERS: '/members',
+    ABSENCES: '/absences',
+    ABSENCE_TYPES: '/absences/types'
 };
 
+router.get(ROUTES.MEMBERS, getMembers);
+
+router.get(ROUTES.ABSENCES, getAbsences);
+
+router.get(ROUTES.ABSENCE_TYPES, getUniqueAbsenceTypes);
+
 module.exports = router;
+
